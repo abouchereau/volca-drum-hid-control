12a222be5c59c04c6f6ac077bc540586de5ccdb2
anto <anthonybouchereau@hotmail.com>
2021-10-22 09:18:08 +0200

    log midi to file

--- replay-from-log.js
+++ replay-from-log.js
@@ -1,1 +1,25 @@
 const fs = require('fs');
+const MidiNode = require("./MidiNode");
+const Utils = require("./Utils");
+const readline = require('readline');
+
+const LOG_DIRECTORY = './log';
+
+let myArgs = process.argv.slice(2);
+let filename = myArgs[0];
+let logTxt = "";
+let logTab = [];
+
+
+readInterface.on('line', function(line) {
+    console.log(line);
+});
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream(LOG_DIRECTORY+"/"+filename),
+    output: process.stdout,
+    console: false
+});
+
+
+
