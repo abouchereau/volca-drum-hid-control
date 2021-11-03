e4b52d473b3f88664780aedbdba46d3b597ba6be
anto <anthonybouchereau@hotmail.com>
2021-11-03 22:08:30 +0100

    log midi to file

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gnome-terminal --profile=profil1 --geometry=32x12+10+10 -- bash -c "node volca-drum-hid-control.js"
+gnome-terminal --profile=profil1 --geometry=34x14+10+10 -- bash -c "sudo node volca-drum-hid-control.js"
