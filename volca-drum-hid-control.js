2c9b07e4ed5a9b25589fd05d18dc5c7d3460bf38
anto <anthonybouchereau@hotmail.com>
2021-11-29 22:18:11 +0100

    logs parameters

--- volca-drum-hid-control.js
+++ volca-drum-hid-control.js
@@ -6,28 +6,58 @@ const MidiNode = require("./lib/MidiNode");
 const VolcaDrum = require("./lib/VolcaDrum");
 const DeviceDJHeroPS = require("./lib/DeviceDJHeroPS");
 const DeviceArcadeJoystick = require("./lib/DeviceArcadeJoystick");
+const yargs = require('yargs');
+
+const argv = yargs
+    .option("midiOutputIndex", {
+        alias: "o",
+        describe: "The index of the Midi Output",
+        type: 'number',
+    })
+    .option("logToFile", {
+        alias: "lf",
+        describe: "Log to file",
+        type:'boolean'})
+    . option("logToScreen", {
+        alias: "ls",
+        describe: "Log to Screen",
+        type:'boolean'
+    }).argv;
+
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//console.log(argv.midiOutputIndex, argv.logToFile, argv.logToScreen);
+
+
 let midiNode = new MidiNode();
 midiNode.scanOutput();
-let volca = new VolcaDrum(midiNode, process.argv.slice(2));
+let volca = new VolcaDrum(midiNode, argv.logToFile, argv.logToScreen);
 
-rl.question("=> Choose MIDI Output (default: "+midiNode.DEFAULT_MIDI_INDEX+") : ", paramMidiIndex => {
-    midiNode.midiOutIndex = midiNode.DEFAULT_MIDI_INDEX;
-    if (paramMidiIndex == null || paramMidiIndex == "" || parseInt(paramMidiIndex) > (midiNode.nbMidiDevices - 1)) {
-        rl.question("=> Bad index for MIDI Output", a=>process.exit());
-    }
-    midiNode.midiOutIndex = parseInt(paramMidiIndex);
+if(typeof argv.midiOutputIndex == "number") {
+    start(argv.midiOutputIndex);
+}
+else {
+    rl.question("=> Choose MIDI Output (default: " + midiNode.DEFAULT_MIDI_INDEX + ") : ", paramMidiIndex => {
+        let midiOutIndex = midiNode.DEFAULT_MIDI_INDEX;
+        if (paramMidiIndex == null || paramMidiIndex == "" || parseInt(paramMidiIndex) > (midiNode.nbMidiDevices - 1)) {
+            rl.question("=> Bad index for MIDI Output", a => process.exit());
+        }
+        midiOutIndex = parseInt(paramMidiIndex);
+        start(midiOutIndex);
+    });
+}
+
+function start(midiOutIndex) {
+    midiNode.midiOutIndex = midiOutIndex;
     midiNode.openOutput();
     volca.logger.clear();
     volca.onSettingChange();
-    let deviceDJHero = new DeviceDJHeroPS(volca);
-    let deviceArcadeJoystick = new DeviceArcadeJoystick(volca);
+    let deviceDJHero = new DeviceDJHeroPS(volca, argv.logToScreen);
+    let deviceArcadeJoystick = new DeviceArcadeJoystick(volca, argv.logToScreen);
     deviceDJHero.init();
     deviceArcadeJoystick.init();
-});
-
+}
